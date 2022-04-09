651390440ee5c0ff6932000f710e0148abb4baf7
Paweł Ciszewski <kardahim@gmail.com>
2022-04-10 01:05:19 +0200

    login form complete (frontend)

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import './App.scss'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
-import Register from './pages/Register/Register'
 
 function App() {
   return (
@@ -15,13 +14,14 @@ function App() {
             Główna
           </Link>
           <Link to="/login">Logowanie</Link>
-          <Link to="/register">Rejestracja</Link>
+          <Link to="#">Empty</Link>
+          <Link to="#">Empty</Link>
+          <Link to="#">Empty</Link>
         </nav>
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
           </Routes>
         </main>
         <footer>
--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,9 +1,61 @@
 import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 import './Login.scss'
 
 function Login() {
+    const initialValues = {
+        email: '',
+        password: ''
+    }
+
+    const validationSchema = Yup.object().shape({
+        email: Yup.string().email("To nie jest email").required("To pole jest wymagane"),
+        password: Yup.string()
+            .required("To pole jest wymagane")
+            .matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, "Musi zawierać 8 znaków, wielką i małą literę, liczbę oraz znak specjalny")
+    })
+
+    const onSubmit = (data) => {
+        // TODO: pin to server
+        console.log(data)
+    }
+
+
     return (
-        <div>Login</div>
+        <div className='login-container'>
+            <Formik
+                initialValues={initialValues}
+                onSubmit={onSubmit}
+                validationSchema={validationSchema}>
+                <Form className='login-form'>
+                    <div className='form-header'>logowanie</div>
+                    <div className='form-body'>
+                        <div className='login-icon-container'>
+                            <i className="fa-solid fa-user"></i>
+                        </div>
+                        <div>
+                            <ErrorMessage name='email' component='span' />
+                            <Field
+                                name='email'
+                                className='login-input'
+                                placeholder='Użytkownik' />
+                        </div>
+                        <div>
+                            <ErrorMessage name='password' component='span' />
+                            <Field
+                                name='password'
+                                type='password'
+                                className='login-input'
+                                placeholder='Hasło' />
+                        </div>
+                        <div>
+                            <button type='submit'>Zaloguj się</button>
+                        </div>
+                    </div>
+                </Form>
+            </Formik>
+        </div>
     )
 }
 
