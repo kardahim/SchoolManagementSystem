3821343ccc670f14a47fa28d3472acbe404895fb
Paweł Ciszewski <kardahim@gmail.com>
2022-04-09 14:41:38 +0200

    add frontend routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,18 @@
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
+
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
